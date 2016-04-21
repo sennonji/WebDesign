097d7727855c3dd8e236b6885b6360d4d0930474
Borisjain <borisjain@outlook.com>
2016-04-21 18:32:41 +0900

    *

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016-04-20.
+ */
+$(document).ready(function(){
+    $("#loginbox").hide();
+    $("#loginbtn").click(function(){
+            $("#loginbox").toggle(100);
+    })
+})
